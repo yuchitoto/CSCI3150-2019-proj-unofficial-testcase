4aca4e1648fe0afe3aae181f05869f90d4cb5ab2
Yu Chi To <yuchitoto@gmail.com>
2019-12-19 20:04:39 +0800

    up to superblock init

--- generator/generator.cpp
+++ generator/generator.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "../fast-cpp-csv-parser/csv.h"
 #include "../superblock.h"
 #include "../inode.h"
 
+#define KB 1024
+#define MB 1024*KB
+
 superblock* read_sb(void);
+void write_bs(int fd);
+void write_sb(int fd, superblock *sb);
 
 int main(void)
 {
   superblock *sb = read_sb();
+  int fd = open("./HB", O_RDWR | O_CREAT | O_TRUNC);
+  write_bs(fd);
+  write_sb(fd, sb);
   return 0;
+}
+
+void write_sb(int fd, superblock *sb)
+{
+  lseek(fd, SB_OFFSET, SEEK_SET);
+  write(fd,sb,sizeof(superblock));
+}
+
+void write_bs(int fd)
+{
+  char *boot = (char*)malloc(INODE_OFFSET);
+  memset(boot,0,INODE_OFFSET);
+  write(fd,boot,INODE_OFFSET);
 }
 
 superblock* read_sb(void)
